55fe3508a22074f2029f9374e082743d8036dde0
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-05-20 18:39:10 -0700

    export_to_JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,14 +5,14 @@ import requests as r
 import sys
 
 if __name__ == '__main__':
-    USER_ID = sys.argv[1]
-    URL = "https://jsonplaceholder.typicode.com/"
-    USR = r.get(URL + "users/{}".format(USER_ID)).json()
-    USERNAME = USR.get("username")
-    to_do = r.get(URL + "todos", params={"user_id": USER_ID}).json()
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    usr = r.get(url + "users/{}".format(user_id)).json()
+    username = usr.get("username")
+    to_do = r.get(url + "todos", params={"user_id": user_id}).json()
 
-    with open("{}.json".format(USER_ID), "w") as jsonfile:
-        json.dump({USER_ID: [{"task": e.get("title"),
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{"task": e.get("title"),
                               "completed": e.get("completed"),
-                              "username": USERNAME} for e in to_do]},
+                              "username": username} for e in to_do]},
                   jsonfile)
