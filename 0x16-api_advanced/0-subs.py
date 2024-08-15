75a201b29475be94fa75c76024dfbb8190cde5c3
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-08-15 22:59:07 +0000

    /0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
+""" Exporting csv files"""
+import json
 import requests
-from sys import argv
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """ get a number of subscribers in a given subreddit
-    using API https://www.reddit.com/dev/api """
-    headers = {'User-Agent': 'Yony'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code == requests.codes.ok:
-        res_json = res.json()
-        subscribers = res_json['data']['subscribers']
+    """Read reddit API and return number subscribers """
+    username = 'ledbag123'
+    password = 'Reddit72'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False)
+    if r.status_code == 200:
+        return (r.json()["data"]["subscribers"])
     else:
-        subscribers = 0
-    return subscribers
+        return(0)
