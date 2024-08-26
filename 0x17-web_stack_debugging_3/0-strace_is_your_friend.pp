274c67d1a472fa31b72dd7786a0e71a8d05722c4
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-08-26 00:47:33 +0000

    0x17-web_stack_debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # wp-settings
-exec {'modify file':
-command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
-path    => '/bin/',
+exec { 'fix':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
